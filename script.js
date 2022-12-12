a4dee922b192a219ca3c02b6cf845f3cdc7ad171
Axel Arrieta <axelfas08@gmail.com>
2022-12-12 14:36:22 -0400

    menu responsive y modificaciones background

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainMenuElement = document.getElementById('main-menu');
+const linkMenu = document.getElementById('main-menu__link');
+
+toggleMenuElement.addEventListener('click', () =>{
+    mainMenuElement.classList.toggle('main-menu--show')
+})
+
+linkMenu.addEventListener('click', () =>{
+    mainMenuElement.classList.toggle('main-menu--show')
+})
